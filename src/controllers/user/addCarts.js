4cb66a4ccc5465e37d5db30190509c8de17e36f7
anandhuc1666 <anandhuc166@gmail.com>
2025-10-16 15:27:49 +0530

    FreshBite-server

--- src/controllers/user/addCarts.js
+++ src/controllers/user/addCarts.js
@@ -114,23 +114,31 @@
 
 import User from "../../models/userSchema.js";
 import Product from "../../models/productSchema.js";
+import CustomError from "../../utils/customError.js";
 
 export const cartAdd = async (req, res, next) => {
-  const { id } = req.params;
-  console.log(id);
+  const { productId } = req.params;
+  console.log(productId);
   
-  const product = await Product.findById(id);
+  const product = await Product.findById(productId);
   if (!product) {
     return next(new CustomError("product not found", 404));
   }
+  
   const userId = req.user.id;
   console.log(userId);
   
   const user = await User.findById(userId);
-  if (!user) {
+    if (!user) {
     return next(new CustomError("user not found", 404));
   }
-   user.cart.push({
+  const existingItem = user.cart.find((item)=>item.productId.toString() === productId)
+  if(existingItem){
+       existingItem.quantity += 1;
+        await user.save();
+    return res.status(200).json({message:"item quantity increase"})
+  }else{
+  user.cart.push({
         productId: product._id,
         img: product.img,
         item: product.item,
@@ -141,5 +149,38 @@ export const cartAdd = async (req, res, next) => {
         quantity: 1,
       });
     await user.save()
-  res.status(200).json({ message: "product found", product });
+  res.status(200).json({ message: "product add the user cart",});
+}
 };
+
+//item already on the cart the item count will increase
+
+
+
+
+
+
+
+
+//item is delete on the related id on the given url
+export const deletCart = async(req,res,next)=>{
+  const {productId} = req.params;
+  const userId = req.user.id
+  const user = await User.findById(userId)
+  if(!user){
+    return next(new CustomError("user not found",404))
+  }
+  const existItem = user.cart.find((item)=>item.productId.toString() === productId)
+    if (!existItem) {
+    return next(new CustomError("Item not found in cart", 404));
+  }
+
+    user.cart = user.cart.filter((item)=>item.productId.toString() !== productId)
+    await user.save();
+
+  res.status(200).json({
+    message: "Item deleted from cart",
+    cart: user.cart,
+  });
+  
+}
--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,24 +1,12 @@
-// import express from "express";
-// import verifyUser from "../middlewares/verifyUser.js";
-// import { addToCart, getCart, removeFromCart } from "../controllers/cartController.js";
-
-// const router = express.Router();
-
-// router.post("/add/:productId", verifyUser, addToCart);
-// router.get("/", verifyUser, getCart);
-// router.delete("/remove/:productId", verifyUser, removeFromCart);
-
-// export default router;
-
-
 import express from "express"
-import { cartAdd } from "../controllers/user/addCarts.js";
+import { cartAdd,deletCart } from "../controllers/user/addCarts.js";
 import trycatch from "../middlewares/tryCatch.js";
 import verifyUser from "../middlewares/auth.js";
+
 
 const routes = express.Router()
 
 routes
-.post('/cartAdd/:id',verifyUser,trycatch(cartAdd))
-
+.post('/cartAdd/:productId',verifyUser,trycatch(cartAdd))
+.delete('/cartDlt/:productId',verifyUser,trycatch(deletCart))
 export default routes
