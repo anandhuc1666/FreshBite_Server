278099ad38010e2859a68440548632fef3430882
anandhuc1666 <anandhuc166@gmail.com>
2025-10-18 11:01:28 +0530

    FreshBite-server

--- src/controllers/user/addFav.js
+++ src/controllers/user/addFav.js
@@ -3,64 +3,71 @@ import Product from "../../models/productSchema.js";
 import CustomError from "../../utils/customError.js";
 
 // add item the perticuler user fav array
-export const addFav = async(req,res,next)=>{
-    const userId = req.user.id
-    const { productId } = req.params;
-    const user = await User.findById(userId)
-  if(!user){
-   return next(new CustomError("user not found",404))
+export const addFav = async (req, res, next) => {
+  const userId = req.user.id;
+  const { productId } = req.params;
+  const user = await User.findById(userId);
+  if (!user) {
+    return next(new CustomError("user not found", 404));
   }
-  const product = await Product.findById(productId)
-  const favUser = user.fav
-  const existeItem = favUser.find((item)=>item.productId.toString() === productId)
-  if(existeItem){
-    return res.status(404).json({message:"product on the fav"})
-  }else{
- user.fav.push({
-     productId: product._id,
-        img: product.img,
-        item: product.item,
-        rate: product.rate,
-        cat: product.cat,
-        detail: product.detail,
-        price: product.price,
-        quantity: 1,
- })
-   await user.save(),
-   res.status(200).json({message:"user fav item add",product})
+  const product = await Product.findById(productId);
+  const favUser = user.fav;
+  const existeItem = favUser.find(
+    (item) => item.productId.toString() === productId
+  );
+  if (existeItem) {
+    return res.status(404).json({ message: "product on the fav" });
+  } else {
+    user.fav.push({
+      productId: product._id,
+      img: product.img,
+      item: product.item,
+      rate: product.rate,
+      cat: product.cat,
+      detail: product.detail,
+      price: product.price,
+      quantity: 1,
+    });
+    await user.save(),
+      res.status(200).json({ message: "user fav item add", product });
   }
-    
-}
+};
 //show user all fav on the list
 
-export const getFav = async(req,res,next)=>{
-    const userId = req.user.id
-    const user = await User.findById(userId)
-    if(!user){
-        return next(new CustomError("user not found",404))
-    }
-    const UserFav = user.fav;
-    if(!UserFav || UserFav.length === 0){
-        return next(new CustomError("user fav empty",404))
-    }
-    res.status(200).json({
-        message:"user fav items",
-        fav: UserFav
-    })
-}
+export const getFav = async (req, res, next) => {
+  const userId = req.user.id;
+  const user = await User.findById(userId);
+  if (!user) {
+    return next(new CustomError("user not found", 404));
+  }
+  const UserFav = user.fav;
+  if (!UserFav || UserFav.length === 0) {
+    return next(new CustomError("user fav empty", 404));
+  }
+  res.status(200).json({
+    message: "user fav items",
+    fav: UserFav,
+  });
+};
 //delete the related product id on the list
 
-// export const deltCart = async(req,res)=>{
-//   const userId = req.user.id;
-//   const user = await User.findById(userId)
-//   const productId = req.params
-//   const userCart = user.cart
-//   const findItem = userCart.find((item)=>item.productId.toString()===productId) 
-//   userCart = userCart.filter((item)=>item.productId !== findItem.id)
-//   if(userCart){
-//     return res.status(200).json({message:"your item is delete",
-//       cart: user.cart  
-//     })
-//   }
-//   await userCart.save()
-// }
+export const favDelt = async (req, res, next) => {
+  const userId = req.user.id;
+  const { productId } = req.params;
+  const user = await User.findById(userId);
+  console.log(user.fav);
+  if (!user) return res.status(404).json({ message: "User not found" });
+  if (!user.fav) return res.status(400).json({ message: "Fav list is empty" });
+  const takeFave = user.fav.find(
+    (item) => item.productId.toString() === productId
+  );
+  if (!takeFave) {
+    return next(new CustomError("user fav not found!", 404));
+  }
+  user.fav = user.fav.filter((item) => item.productId.toString() !== productId);
+  if (!user.fav) {
+    return next(new CustomError("user cannot be delete the item's"));
+  }
+  res.status(200).json({ message: "user fave item deleted" });
+  await user.save();
+};
